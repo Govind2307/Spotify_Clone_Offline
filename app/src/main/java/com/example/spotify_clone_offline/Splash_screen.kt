60e819a94b4ac97de69a87ecb29021e9929589f9
hp <govindpurohit@gmail.com>
2022-06-30 14:57:25 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/spotify_clone_offline/Splash_screen.kt
@@ -0,0 +1,22 @@
+package com.example.spotify_clone_offline
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+        Handler().postDelayed(Runnable {
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+
+        }, 3000)
+
+
+    }
+}
